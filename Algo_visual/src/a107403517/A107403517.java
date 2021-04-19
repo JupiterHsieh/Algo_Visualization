18089a64a30c2754c9559980ad326dab153089f4
JupiterHsieh <taipeiallen15@gmail.com>
2021-04-19 22:44:38 +0800

    First commit

---
+++ Algo_visual/src/a107403517/A107403517.java
@@ -0,0 +1,30 @@
+package a107403517;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.io.*;
+import java.util.Arrays;
+
+class MainGUI{
+	JFrame frame;
+	JPanel panel1;
+	public MainGUI() {
+		frame = new JFrame();
+		panel1 = new JPanel();
+		
+	}
+}
+
+public class A107403517 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("107403517_sorting");
+		frame.setResizable(false);
+		frame.setSize(420,420);
+		frame.setVisible(true);
+		
+	}
+}
+ 
